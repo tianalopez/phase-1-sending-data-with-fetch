710acb96c6c99180ce69542d14553310f4d5d2d2
Tiana Lopez <tianadavin@gmail.com>
2023-09-16 13:28:05 -0700

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+    })
+  })
+  .then((resp) => (resp.json()))
+  .then(newObject => {
+    // document.body.innerHTML = newObject[id]
+    // let id = document.querySelector(`${newObject}`).id
+    document.body.innerHTML = newObject['id']
+  })
+  .catch(error => {
+    document.body.innerHTML = error.message;
+  })
+}
